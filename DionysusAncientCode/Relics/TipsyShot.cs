fb7389529eb78d04c97d36cf6199c917dc643600
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-05-14 20:20:22 +0200

    Implement Tipsy Shot.

---
+++ DionysusAncientCode/Relics/TipsyShot.cs
@@ -0,0 +1,11 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+
+namespace DionysusAncient.DionysusAncientCode.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class TipsyShot : DionysusAncientRelic
+{
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+}
